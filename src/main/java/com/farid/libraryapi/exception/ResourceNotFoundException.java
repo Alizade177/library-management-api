50577fc1d2394cfb47ab09bcf737716ffaf25de7
Alizade177 <ferid.elizade65@gmail.com>
2026-07-20 13:28:17 +0400

    Add global exception handling

---
+++ src/main/java/com/farid/libraryapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.farid.libraryapi.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
