b876e46a8d3b5a9a70093649810e62daf565d7fb
Alizade177 <ferid.elizade65@gmail.com>
2026-07-29 22:34:17 +0400

    Configure Spring Security and stateless session

--- src/main/java/com/farid/libraryapi/config/SecurityConfig.java
+++ src/main/java/com/farid/libraryapi/config/SecurityConfig.java
@@ -1,4 +1,76 @@
 package com.farid.libraryapi.config;
 
+import com.farid.libraryapi.repository.UserRepository;
+import com.farid.libraryapi.security.CustomAccessDeniedHandler;
+import com.farid.libraryapi.security.CustomAuthenticationEntryPoint;
+import com.farid.libraryapi.security.JwtFilter;
+import com.farid.libraryapi.security.JwtService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public JwtFilter jwtFilter(
+            JwtService jwtService,
+            UserRepository userRepository) {
+
+        return new JwtFilter(
+                jwtService,
+                userRepository
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            JwtFilter jwtFilter,
+            CustomAuthenticationEntryPoint authenticationEntryPoint,
+            CustomAccessDeniedHandler accessDeniedHandler)
+            throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(
+                                authenticationEntryPoint
+                        )
+                        .accessDeniedHandler(
+                                accessDeniedHandler
+                        )
+                )
+
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(
+                                "/auth/register",
+                                "/auth/login"
+                        ).permitAll()
+
+                        .anyRequest().authenticated()
+                )
+
+                .addFilterBefore(
+                        jwtFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
+
+        return http.build();
+    }
 }
