930fdee8b7f4a331bee83e80c3f4b92ebe6862e6
Alizade177 <ferid.elizade65@gmail.com>
2026-07-29 22:33:52 +0400

    Add login and JWT token generation

--- src/main/java/com/farid/libraryapi/security/JwtService.java
+++ src/main/java/com/farid/libraryapi/security/JwtService.java
@@ -1,4 +1,72 @@
 package com.farid.libraryapi.security;
 
+import com.farid.libraryapi.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Service
 public class JwtService {
+
+    private final Key key;
+    private final long expirationTime;
+
+    public JwtService(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") long expirationTime) {
+
+        this.key = Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8)
+        );
+
+        this.expirationTime = expirationTime;
+    }
+
+    public String generateToken(User user) {
+
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .claim("role", user.getRole().name())
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(
+                                System.currentTimeMillis()
+                                        + expirationTime
+                        )
+                )
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+
+        try {
+            getClaims(token);
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
