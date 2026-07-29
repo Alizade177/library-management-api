fe7893e679936731b023e6b32d560681319ec572
Alizade177 <ferid.elizade65@gmail.com>
2026-07-29 22:31:54 +0400

    Add User entity and BCrypt password encoding

--- src/main/java/com/farid/libraryapi/entity/User.java
+++ src/main/java/com/farid/libraryapi/entity/User.java
@@ -1,4 +1,37 @@
 package com.farid.libraryapi.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
