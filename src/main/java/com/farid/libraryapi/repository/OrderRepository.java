8037f49848666013100fc5375bb4824c38f2fcf6
Alizade177 <ferid.elizade65@gmail.com>
2026-08-06 13:40:36 +0400

    Fix N+1 problem using EntityGraph

--- src/main/java/com/farid/libraryapi/repository/OrderRepository.java
+++ src/main/java/com/farid/libraryapi/repository/OrderRepository.java
@@ -1,7 +1,27 @@
 package com.farid.libraryapi.repository;
 
 import com.farid.libraryapi.entity.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @EntityGraph(attributePaths = {
+            "member",
+            "items",
+            "items.book"
+    })
+    List<Order> findAllWithEntityGraph();
+
+    @Query("""
+        select distinct o
+        from Order o
+        join fetch o.member
+        join fetch o.items i
+        join fetch i.book
+        """)
+    List<Order> findAllWithItems();
 }
---
+++ src/main/java/com/farid/libraryapi/specification/BookSpecification.java
@@ -0,0 +1,4 @@
+package com.farid.libraryapi.specification;
+
+public class BookSpecification {
+}
