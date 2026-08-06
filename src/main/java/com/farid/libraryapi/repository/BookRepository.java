38f8a711cd14a09c2d184a12e108d0800ff16554
Alizade177 <ferid.elizade65@gmail.com>
2026-08-06 13:40:55 +0400

    Add Specification support for book search

--- src/main/java/com/farid/libraryapi/repository/BookRepository.java
+++ src/main/java/com/farid/libraryapi/repository/BookRepository.java
@@ -1,12 +1,11 @@
 package com.farid.libraryapi.repository;
 
 import com.farid.libraryapi.entity.Book;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
-
-public interface BookRepository extends JpaRepository<Book,Long> {
+public interface BookRepository
+        extends JpaRepository<Book, Long>,
+        JpaSpecificationExecutor<Book> {
 
 }
--- src/main/java/com/farid/libraryapi/specification/BookSpecification.java
+++ src/main/java/com/farid/libraryapi/specification/BookSpecification.java
@@ -1,4 +1,113 @@
 package com.farid.libraryapi.specification;
 
+import com.farid.libraryapi.entity.Book;
+import jakarta.persistence.criteria.Join;
+import org.springframework.data.jpa.domain.Specification;
+
 public class BookSpecification {
+
+    public static Specification<Book> hasTitle(String title){
+
+        return (root, query, cb)->
+
+                title == null ?
+
+                        null :
+
+                        cb.like(
+
+                                cb.lower(root.get("title")),
+
+                                "%" + title.toLowerCase() + "%"
+
+                        );
+
+    }
+
+    public static Specification<Book> hasAuthor(String author){
+
+        return (root, query, cb)->{
+
+            if(author == null){
+
+                return null;
+
+            }
+
+            Join<Object,Object> join =
+                    root.join("author");
+
+            return cb.like(
+
+                    cb.lower(join.get("name")),
+
+                    "%" + author.toLowerCase() + "%"
+
+            );
+
+        };
+
+    }
+
+    public static Specification<Book> hasCategory(String category){
+
+        return (root, query, cb)->{
+
+            if(category == null){
+
+                return null;
+
+            }
+
+            Join<Object,Object> join =
+                    root.join("categories");
+
+            return cb.like(
+
+                    cb.lower(join.get("name")),
+
+                    "%" + category.toLowerCase() + "%"
+
+            );
+
+        };
+
+    }
+
+    public static Specification<Book> minPrice(Double minPrice){
+
+        return (root, query, cb)->
+
+                minPrice == null ?
+
+                        null :
+
+                        cb.greaterThanOrEqualTo(
+
+                                root.get("price"),
+
+                                minPrice
+
+                        );
+
+    }
+
+    public static Specification<Book> maxPrice(Double maxPrice){
+
+        return (root, query, cb)->
+
+                maxPrice == null ?
+
+                        null :
+
+                        cb.lessThanOrEqualTo(
+
+                                root.get("price"),
+
+                                maxPrice
+
+                        );
+
+    }
+
 }
