95b69d1ddb8eec51c7921bfab485d95a4ece8687
Alizade177 <ferid.elizade65@gmail.com>
2026-08-15 22:55:23 +0400

    fix: resolve n plus one query with entity graph

--- src/main/java/com/farid/libraryapi/repository/OrderRepository.java
+++ src/main/java/com/farid/libraryapi/repository/OrderRepository.java
@@ -4,25 +4,26 @@ import com.farid.libraryapi.entity.Order;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Override
     @EntityGraph(attributePaths = {
             "member",
             "items",
             "items.book"
     })
-    List<Order> findAll();
+    @Query("select o from Order o")
+    List<Order> findAllWithEntityGraph();
 
-    @Query("""
-    select distinct o
-    from Order o
-    join fetch o.member
-    join fetch o.items i
-    join fetch i.book
-    """)
-    List<Order> findAllWithItems();
+    @EntityGraph(attributePaths = {
+            "member",
+            "items",
+            "items.book"
+    })
+    @Query("select o from Order o where o.id = :id")
+    Optional<Order> findByIdWithEntityGraph(@Param("id") Long id);
 }
